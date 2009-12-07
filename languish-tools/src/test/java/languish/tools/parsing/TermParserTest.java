cb3a75a41c06d16b49d3ef91f665e1e9ebac351a
hjfreyer <devnull@localhost>
2009-12-06 21:07:44 -0500

    Fixed test

--- languish-tools/src/test/java/languish/tools/parsing/TermParserTest.java
+++ languish-tools/src/test/java/languish/tools/parsing/TermParserTest.java
@@ -8,42 +8,47 @@ import com.google.common.collect.ImmutableList;
 import com.hjfreyer.util.Tree;
 
 public class TermParserTest extends TestCase {
+  @SuppressWarnings("unchecked")
   public void testNull() {
-    Tree<String> ast = TermParser.TERM_GRAMMAR.getAstParser().parse("NULL");
+    Tree<String> expected =
+        Tree.inode(Tree.leaf("NULL_TERM"), Tree.inode(Tree.inode(Tree
+            .leaf("NULL"), Tree.leaf("NULL"))));
 
-    List<?> expected = ImmutableList.of("NULL_TERM", ImmutableList.of("NULL"));
-
-    assertEquals(Tree.copyOf(expected), ast);
+    assertEquals(expected, TermParser.TERM_GRAMMAR.getAstParser().parse("NULL"));
   }
 
+  @SuppressWarnings("unchecked")
   public void testStringPrimtive() {
     Tree<String> ast =
         TermParser.TERM_GRAMMAR.getAstParser().parse(
             "[PRIMITIVE \"foobar\" NULL]");
 
-    List<?> expected =
-        ImmutableList.of("PRIMITIVE_TERM", ImmutableList.of(
-            "[",
-            "PRIMITIVE",
-            ImmutableList.of("STRING", ImmutableList.of("\"foobar\"")),
-            ImmutableList.of("NULL_TERM", ImmutableList.of("NULL")),
-            "]"));
+    List<?> expected = ImmutableList.of("PRIMITIVE_TERM", ImmutableList.of( //
+        ImmutableList.of("[", "["),
+        ImmutableList.of("PRIMITIVE", "PRIMITIVE"),
+        ImmutableList.of("STRING", ImmutableList.of( //
+            ImmutableList.of("STRING_LIT", "\"foobar\""))),
+        ImmutableList.of("NULL_TERM", ImmutableList.of( //
+            ImmutableList.of("NULL", "NULL"))),
+        ImmutableList.of("]", "]")));
 
     assertEquals(Tree.copyOf(expected), ast);
   }
 
+  @SuppressWarnings("unchecked")
   public void testIntPrimtive() {
     Tree<String> ast =
         TermParser.TERM_GRAMMAR.getAstParser().parse("[PRIMITIVE 42 NULL]");
 
-    List<?> expected =
-        ImmutableList.of("PRIMITIVE_TERM", ImmutableList.of(
-            "[",
-            "PRIMITIVE",
-            ImmutableList.of("INTEGER", ImmutableList.of("42")),
-            ImmutableList.of("NULL_TERM", ImmutableList.of("NULL")),
-            "]"));
+    List<?> expected = ImmutableList.of("PRIMITIVE_TERM", ImmutableList.of( //
+        ImmutableList.of("[", "["),
+        ImmutableList.of("PRIMITIVE", "PRIMITIVE"),
+        ImmutableList.of("INTEGER", ImmutableList.of( //
+            ImmutableList.of("INTEGER_LIT", "42"))),
+        ImmutableList.of("NULL_TERM", ImmutableList.of( //
+            ImmutableList.of("NULL", "NULL"))),
+        ImmutableList.of("]", "]")));
 
-    assertEquals(Tree.copyOf(expected), ast);
+    assertEquals(Tree.copyOf(expected).toString(), ast.toString());
   }
 }
