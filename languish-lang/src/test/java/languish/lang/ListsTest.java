5f99a5f33313b2140dc5f444a41bb1b8e383051d
hjfreyer <devnull@localhost>
2009-12-19 19:56:00 -0500

    Uncommented list tests

--- languish-lang/src/test/java/languish/lang/ListsTest.java
+++ languish-lang/src/test/java/languish/lang/ListsTest.java
@@ -96,98 +96,105 @@ public class ListsTest extends TestCase {
         list), primitive(ONE)));
   }
 
-  // public void testReduceSumManyElements() {
-  // Term list =
-  // Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
-  // 4,
-  // 5,
-  // 6,
-  // 7)));
-  //
-  // TestUtil.assertReducesToData(PrimitiveTree.copyOf(23), app(app(
-  // SUM_REDUCE,
-  // list), primitive(ONE)));
-  // }
-  //
-  // public void testReduceNonCommutative() {
-  // Term list = Util.convertJavaObjectToTerm(ImmutableList.of(4, 5, 6, 7));
-  //
-  // TestUtil.assertReducesToData(LInteger.of(4), app(app(app(
-  // Lists.reduce(),
-  // abs(abs(ref(2)))), list), primitive(LInteger.of(1))));
-  //
-  // TestUtil.assertReducesToData(LInteger.of(1), app(app(app(
-  // Lists.reduce(),
-  // abs(abs(ref(1)))), list), primitive(LInteger.of(1))));
-  // }
-  //
-  // public void testMemberEmptyList() {
-  // Term list = Util.convertJavaObjectToTerm(ImmutableList.of());
-  //
-  // TestUtil.assertReducesToData(LBoolean.FALSE, app(
-  // app(Lists.member(), list),
-  // primitive(LSymbol.of("foo"))));
-  // }
-  //
-  // public void testMemberOnlyElement() {
-  // Term list = Util.convertJavaObjectToTerm(ImmutableList.of("foo"));
-  //
-  // TestUtil.assertReducesToData(LBoolean.TRUE, app(
-  // app(Lists.member(), list),
-  // primitive(LSymbol.of("foo"))));
-  // }
-  //
-  // @SuppressWarnings("unchecked")
-  // public void testMemberShouldReturnTrue() {
-  // Term list = Util.convertJavaObjectToTerm(ImmutableList.of("foo", "bar",
-  // 5));
-  //
-  // TestUtil.assertReducesToData(LBoolean.TRUE, app(
-  // app(Lists.member(), list),
-  // primitive(LSymbol.of("foo"))));
-  // }
-  //
-  // @SuppressWarnings("unchecked")
-  // public void testMemberNonemptyShouldReturnFalse() {
-  // Term list = Util.convertJavaObjectToTerm(ImmutableList.of("foo", "bar",
-  // 5));
-  //
-  // TestUtil.assertReducesToData(LBoolean.FALSE, app(
-  // app(Lists.member(), list),
-  // primitive(LSymbol.of("baz"))));
-  // }
-  //
-  // public void testMemberShouldContainEmptyList() {
-  // Term list =
-  // Util.convertJavaObjectToTerm(ImmutableList.of(
-  // "foo",
-  // ImmutableList.of(),
-  // 5));
-  //
-  // TestUtil.assertReducesToData(LBoolean.TRUE, app(
-  // app(Lists.member(), list),
-  // primitive(Term.of())));
-  // }
-  //
-  // public void testMemberShouldntFindInNested() {
-  // Term list =
-  // Util.convertJavaObjectToTerm(ImmutableList.of("foo", ImmutableList.of(
-  // 3,
-  // 4), 5));
-  //
-  // TestUtil.assertReducesToData(LBoolean.FALSE, app(
-  // app(Lists.member(), list),
-  // primitive(LInteger.of(3))));
-  // }
-  //
-  // public void testMemberShouldContainNonEmptyList() {
-  // Term list =
-  // Util.convertJavaObjectToTerm(ImmutableList.of("foo", ImmutableList.of(
-  // 3,
-  // 4), 5));
-  //
-  // TestUtil.assertReducesToData(LBoolean.TRUE, app(
-  // app(Lists.member(), list),
-  // Util.convertJavaObjectToTerm(ImmutableList.of(3, 4))));
-  // }
+  public void testReduceSumManyElements() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            4,
+            5,
+            6,
+            7)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(23), app(app(
+        SUM_REDUCE,
+        list), primitive(ONE)));
+  }
+
+  public void testReduceNonCommutative() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            4,
+            5,
+            6,
+            7)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(4), app(app(app(Lists
+        .reduce(), abs(abs(ref(2)))), list), primitive(ONE)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(1), app(app(app(Lists
+        .reduce(), abs(abs(ref(1)))), list), primitive(ONE)));
+  }
+
+  public void testMemberEmptyList() {
+    Term list = Term.NULL;
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(false), app(app(Lists
+        .member(), list), primitive(new Primitive("foo"))));
+  }
+
+  public void testMemberOnlyElement() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList
+            .of("foo")));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(true), app(app(Lists
+        .member(), list), primitive(new Primitive("foo"))));
+  }
+
+  @SuppressWarnings("unchecked")
+  public void testMemberShouldReturnTrue() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            "foo",
+            "bar",
+            5)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(true), app(app(Lists
+        .member(), list), primitive(new Primitive("foo"))));
+  }
+
+  @SuppressWarnings("unchecked")
+  public void testMemberNonemptyShouldReturnFalse() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            "foo",
+            "bar",
+            5)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(false), app(app(Lists
+        .member(), list), primitive(new Primitive("baz"))));
+  }
+
+  public void testMemberShouldContainEmptyList() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            "foo",
+            ImmutableList.of(),
+            5)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(true), app(app(Lists
+        .member(), list), Term.NULL));
+  }
+
+  public void testMemberShouldntFindInNested() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            "foo",
+            ImmutableList.of(3, 4),
+            5)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(false), app(app(Lists
+        .member(), list), primitive(THREE)));
+  }
+
+  public void testMemberShouldContainNonEmptyList() {
+    Term list =
+        Terms.convertJavaObjectToTerm(PrimitiveTree.copyOf(ImmutableList.of(
+            "foo",
+            ImmutableList.of(3, 4),
+            5)));
+
+    TestUtil.assertReducesToData(PrimitiveTree.copyOf(true), app(app(Lists
+        .member(), list), Terms.convertJavaObjectToTerm(PrimitiveTree
+        .copyOf(ImmutableList.of(3, 4)))));
+  }
 }
